7d353394be22877882f19aebd3d0ee61d6a04c05
John Hawthorn <john@hawthorn.email>
2024-10-12 15:39:47 -0700

    Add some tests for Rails merge conflicts

--- test/test_bundler_merge.rb
+++ test/test_bundler_merge.rb
@@ -33,13 +33,33 @@ BUNDLED WITH
 LOCKFILE
   end
 
+
+  Dir["#{FIXTURES}/rails/*"].each do |dir|
+    test_name = File.basename(dir)
+    define_method("test_#{test_name}") do
+      result = run_merge(
+        "#{dir}/a",
+        "#{dir}/base",
+        "#{dir}/b",
+      )
+      assert result.status.success?
+
+      expected = File.read("#{dir}/merge")
+      assert_equal expected, result.merged
+    end
+  end
+
   Result = Struct.new(:status, :stdout, :merged)
 
   def run_merge(local, base, remote)
+    local = File.expand_path(local, FIXTURES)
+    base = File.expand_path(base, FIXTURES)
+    remote = File.expand_path(remote, FIXTURES)
+
     Dir.mktmpdir do |tmpdir|
-      FileUtils.cp("#{FIXTURES}/#{local}", "#{tmpdir}/local")
-      FileUtils.cp("#{FIXTURES}/#{base}", "#{tmpdir}/base")
-      FileUtils.cp("#{FIXTURES}/#{remote}", "#{tmpdir}/remote")
+      FileUtils.cp(local, "#{tmpdir}/local")
+      FileUtils.cp(base, "#{tmpdir}/base")
+      FileUtils.cp(remote, "#{tmpdir}/remote")
 
       argv = ["#{tmpdir}/local", "#{tmpdir}/base", "#{tmpdir}/remote"]
       #stdout = IO.popen([RbConfig.ruby, BIN, "", :err=>[:child, :out]]) do |io|
